741f51a463b11e71090bc6404b5506f0143eeb96
James Curtis <james.curtis@kcl.ac.uk>
2022-09-28 17:12:53 +0100

    Add frequency checking feature

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,13 @@
 
 def frequencies(items):
     frequencies = {}
+    for item in items:
+        item = str(item)
+        if(not frequencies.__contains__(item)):
+            frequencies[item] = 1
+        else:
+            frequencies[item] = frequencies[item] + 1
     # Your code goes here
     return frequencies
+
+print(frequencies(["a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "b", "b", "b"]))
